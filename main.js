f24cf8c8f8b87aced8d537c416a9e9012ebdf549
David Kerr <kerrd89@gmail.com>
2016-12-04 12:55:16 -0700

    initial setup

---
+++ main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+
+
+let mainWindow;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    width: 700,
+    height: 300,
+    minWidth: 600,
+    minHeight: 300,
+    show: false
+  });
+
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+  });
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+});
